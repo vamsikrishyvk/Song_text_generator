3e256d05054713626621006194a6b15eaed48de3
yvkvamsikrish <60051414+vamsikrishyvk@users.noreply.github.com>
2023-06-01 12:17:29 +0530

    Update test_gapi.py

--- test_gapi.py
+++ test_gapi.py
@@ -1,13 +1,1 @@
-import requests
-docker_url = 'http://0.0.0.0:8000/song_generator'
-
-response = requests.get(docker_url)
-
-# Check the response status code
-if response.status_code == 200:
-    # Successful API call
-    data = response.json()
-    print('API response:', data)
-else:
-    # Error in API call
-    print('API call failed with status code:', response.status_code)
+#  blank test
